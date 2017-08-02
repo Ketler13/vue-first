8d3a6ad1f9fe0fecf43a842d598e692379fd468e
Ketler <ketler13@yandex.ru>
2017-08-02 22:56:22 +0300

    add class components to login signup

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,27 +1,53 @@
-export default {
-  name: 'login',
-  data () {
-    return {
-      email: '',
-      password: '',
-      logging: false
-    }
-  },
-  computed: {
-    formFullfilled () {
-      return !!this.email && !!this.password && !this.logging
-    }
-  },
-  methods: {
-    login () {
-      this.logging = true
-      this.$store
-        .dispatch('logIn', {email: this.email, password: this.password})
-        .then(() => {
-          this.logging = false
-          this.$router.push({ path: '/newsplit' })
-        })
-        .catch(console.error)
-    }
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+class Login extends Vue {
+  email = ''
+  password = ''
+  logging = false
+
+  get formFullfilled () {
+    return !!this.email && !!this.password && !this.logging
+  }
+
+  login () {
+    this.logging = true
+    this.$store
+      .dispatch('logIn', {email: this.email, password: this.password})
+      .then(() => {
+        this.logging = false
+        this.$router.push({ path: '/newsplit' })
+      })
+      .catch(console.error)
   }
 }
+
+export default Component({name: 'login'})(Login)
+
+// export default {
+//   name: 'login',
+//   data () {
+//     return {
+//       email: '',
+//       password: '',
+//       logging: false
+//     }
+//   },
+//   computed: {
+//     formFullfilled () {
+//       return !!this.email && !!this.password && !this.logging
+//     }
+//   },
+//   methods: {
+//     login () {
+//       this.logging = true
+//       this.$store
+//         .dispatch('logIn', {email: this.email, password: this.password})
+//         .then(() => {
+//           this.logging = false
+//           this.$router.push({ path: '/newsplit' })
+//         })
+//         .catch(console.error)
+//     }
+//   }
+// }
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,87 +1,176 @@
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
 import { _checkName, _checkEmail, pattern } from '@/helpers'
 
-export default {
-  name: 'login',
-  data () {
-    return {
-      name: '',
-      email: '',
-      password: '',
-      emailError: '',
-      nameError: '',
-      passwordError: '',
-      registering: false
-    }
-  },
-  computed: {
-    formFullfilled () {
-      return this.email && this.password && this.name && !this.emailError &&
-      !this.nameError && !this.registering && !this.passwordError
-    }
-  },
-  methods: {
-    signup () {
-      this.registering = true
-      this.$store
-        .dispatch('signUp', {name: this.name, email: this.email, password: this.password})
-        .then(() => {
-          this.registering = false
-          this.$router.push({ path: '/login' })
-        })
-        .catch(console.error)
-    },
-    async checkName (name) {
-      if (!name) {
-        this.nameError = ''
-        return
-      }
-      const pattern = /[a-zA-Z0-9]/
-      if (!pattern.test(name)) {
-        this.nameError = 'username must contain letters and numbers only'
-        return
-      } else {
-        this.nameError = ''
-      }
-      try {
-        const res = await _checkName(name)
-        res.data.success
-        ? this.nameError = ''
-        : this.nameError = 'name already exists'
-      } catch (e) {
-        console.error(e)
-      }
-    },
-    clearNameErrors () {
+class Signup extends Vue {
+  name = ''
+  email = ''
+  password = ''
+  emailError = ''
+  nameError = ''
+  passwordError = ''
+  registering = false
+
+  get formFullfilled () {
+    return this.email && this.password && this.name && !this.emailError &&
+    !this.nameError && !this.registering && !this.passwordError
+  }
+
+  signup () {
+    this.registering = true
+    this.$store
+      .dispatch('signUp', {name: this.name, email: this.email, password: this.password})
+      .then(() => {
+        this.registering = false
+        this.$router.push({ path: '/login' })
+      })
+      .catch(console.error)
+  }
+
+  async checkName (name) {
+    if (!name) {
       this.nameError = ''
-    },
-    async checkEmail (email) {
-      if (!email) {
-        this.emailError = ''
-        return
-      }
-      if (!pattern.test(email)) {
-        this.emailError = 'incorrect email'
-        return
-      } else {
-        this.emailError = ''
-      }
-      try {
-        const res = await _checkEmail(email)
-        res.data.success
-        ? this.emailError = ''
-        : this.emailError = 'email already exists'
-      } catch (e) {
-        console.error(e)
-      }
-    },
-    clearEmailErrors () {
-      this.emailError = ''
-      this.emailError = ''
-    },
-    checkPassword (password) {
-      this.passwordError = (password.length < 6 && password.length !==0 )
-      ? 'password must be at least 6 symbols'
-      : ''
+      return
+    }
+    const pattern = /[a-zA-Z0-9]/
+    if (!pattern.test(name)) {
+      this.nameError = 'username must contain letters and numbers only'
+      return
+    } else {
+      this.nameError = ''
+    }
+    try {
+      const res = await _checkName(name)
+      res.data.success
+      ? this.nameError = ''
+      : this.nameError = 'name already exists'
+    } catch (e) {
+      console.error(e)
     }
   }
+
+  clearNameErrors () {
+    this.nameError = ''
+  }
+
+  async checkEmail (email) {
+    if (!email) {
+      this.emailError = ''
+      return
+    }
+    if (!pattern.test(email)) {
+      this.emailError = 'incorrect email'
+      return
+    } else {
+      this.emailError = ''
+    }
+    try {
+      const res = await _checkEmail(email)
+      res.data.success
+      ? this.emailError = ''
+      : this.emailError = 'email already exists'
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  clearEmailErrors () {
+    this.emailError = ''
+    this.emailError = ''
+  }
+
+  checkPassword (password) {
+    this.passwordError = (password.length < 6 && password.length !==0 )
+    ? 'password must be at least 6 symbols'
+    : ''
+  }
 }
+
+export default Component({name: 'signup'})(Signup)
+
+// export default {
+//   name: 'signup',
+//   data () {
+//     return {
+//       name: '',
+//       email: '',
+//       password: '',
+//       emailError: '',
+//       nameError: '',
+//       passwordError: '',
+//       registering: false
+//     }
+//   },
+//   computed: {
+//     formFullfilled () {
+//       return this.email && this.password && this.name && !this.emailError &&
+//       !this.nameError && !this.registering && !this.passwordError
+//     }
+//   },
+//   methods: {
+//     signup () {
+//       this.registering = true
+//       this.$store
+//         .dispatch('signUp', {name: this.name, email: this.email, password: this.password})
+//         .then(() => {
+//           this.registering = false
+//           this.$router.push({ path: '/login' })
+//         })
+//         .catch(console.error)
+//     },
+//     async checkName (name) {
+//       if (!name) {
+//         this.nameError = ''
+//         return
+//       }
+//       const pattern = /[a-zA-Z0-9]/
+//       if (!pattern.test(name)) {
+//         this.nameError = 'username must contain letters and numbers only'
+//         return
+//       } else {
+//         this.nameError = ''
+//       }
+//       try {
+//         const res = await _checkName(name)
+//         res.data.success
+//         ? this.nameError = ''
+//         : this.nameError = 'name already exists'
+//       } catch (e) {
+//         console.error(e)
+//       }
+//     },
+//     clearNameErrors () {
+//       this.nameError = ''
+//     },
+//     async checkEmail (email) {
+//       if (!email) {
+//         this.emailError = ''
+//         return
+//       }
+//       if (!pattern.test(email)) {
+//         this.emailError = 'incorrect email'
+//         return
+//       } else {
+//         this.emailError = ''
+//       }
+//       try {
+//         const res = await _checkEmail(email)
+//         res.data.success
+//         ? this.emailError = ''
+//         : this.emailError = 'email already exists'
+//       } catch (e) {
+//         console.error(e)
+//       }
+//     },
+//     clearEmailErrors () {
+//       this.emailError = ''
+//       this.emailError = ''
+//     },
+//     checkPassword (password) {
+//       this.passwordError = (password.length < 6 && password.length !==0 )
+//       ? 'password must be at least 6 symbols'
+//       : ''
+//     }
+//   }
+// }
